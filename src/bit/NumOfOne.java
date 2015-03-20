7e9762f98b6b5fa30b734d350888966614fa03bd
Yan He <heyan4869@gmail.com>
2015-03-20 15:20:53 -0400

    Update NumOfOne.java

--- src/bit/NumOfOne.java
+++ src/bit/NumOfOne.java
@@ -1,20 +1,20 @@
 package bit;
 
 public class NumOfOne {
-		public static int countOne(int n) {
-			  int count = 0;
-				for (int i = 0; i < 32; i++) {
-						if ((n & 1) == 1) {
-								count++;		
-						}
-						n = n >> 1;
-				}
-				
-				return count;
+	public static int countOne(int n) {
+		int count = 0;
+		for (int i = 0; i < 32; i++) {
+			if ((n & 1) == 1) {
+				count++;
+			}
+			n = n >> 1;
 		}
 
-		public static void main(String args[]) {
-				int n = 11;
-				System.out.println(countOne(n));
-		}
+		return count;
+	}
+
+	public static void main(String args[]) {
+		int n = 11;
+		System.out.println(countOne(n));
+	}
 }
